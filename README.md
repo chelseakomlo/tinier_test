a84037bcd92086f06001117c54a7c6ac0e51789a
Chelsea H. Komlo <chelsea.komlo@gmail.com>
2017-02-11 00:30:55 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+## Tiny test helper
+
+For very small C unit tests
+
+TODO:
+  - Make file/tasks
---
+++ test_helper.h
@@ -0,0 +1,10 @@
+// include your test files here
+// #include "example_test.c"
+
+#ifndef TEST_HELPER_H
+#define _TEST_HELPER_H
+
+int (*all_tests[])(void) = {
+};
+
+#endif
